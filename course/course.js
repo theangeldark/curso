ae475cb8ff1c66abdbd922392e7f3c9b0b73e564
theangeldark <brandon.lizcano02041@gmail.com>
2024-09-04 08:15:38 -0500

    Add files via upload

---
+++ course/course.js
@@ -0,0 +1,81 @@
+'use strict'
+/*
+document.write("<h1>Hello world</h1>")
+//Tipos de datos
+//string
+"Hello World"
+'Hello World'
+//Number
+10000
+-2.3
+//Boolean
+true
+false
+//Object
+'steven'
+99.99
+35
+//Variables
+var name = "pancracio";
+let lastname = "Leon";
+name = "silvio";
+const PI = 3.1415;
+console.log(name)
+console.log(lastname)
+console.log(PI)
+//Camelcase
+let userName = "AAAA";
+var lastNameOfPerson = "dany";
+//Operados
+let NumberOne = 1;
+let NumberTwo = 2;
+let result = NumberOne + NumberTwo;
+console.log(result);
+let namee = 'mike';
+let lastName = 'Pork';
+let resultt = namee + lastName;
+console.log(resultt);
+let password = 'Notoiletsigma'
+let input = 'Notoiletsigm'
+let resulttt = input == password;
+console.log(resulttt)
+if (resulttt == true) {
+    console.log('Login Correcto')
+} else {
+    console.log('Login Incorrecto')
+}
+let score = 70;
+if (score > 30) {
+    console.log('Ur good')
+} else {
+    console.log('Ur suck')
+}
+let typeCard = 'Debito Card'
+switch (typeCard) {
+    case 'DaviPlata Card':
+    console.log('Trakaa')
+    break;
+    case 'Debit Card':
+    console.log('Yeahhhh')
+    break;
+    default:
+        console.log('What r u using')
+        break;
+}
+//Iteradores
+let count = 1;
+while (count < 50) {
+    console.log(count)
+    count++;
+}
+let names = ['pancracio', 'dany', 'mike'];
+console.log(names.length)
+for(let i = 0; i < names.length; i++){
+ console.log(names[i]);
+}
+//Funciones
+ function greeting(){
+    console.log('Hello')
+ }
+ greeting();
+*/
